a51fba9d53abf7b688f1dedec9da86d2ac6b9ab7
Xiao Grant <xgrant001@gmail.com>
2022-08-25 01:09:16 -0700

    Fixed randomized date issue, updated copy.

--- assets/js/apod.js
+++ assets/js/apod.js
@@ -6,11 +6,10 @@ function randomDate(date1, date2){
     function randomValueBetween(min, max) {
       return Math.random() * (max - min) + min;
     }
-    var date1 = date1 || '06-20-1995'
+    var date1 = date1 || 'June 20, 1995'
     var date2 = date2 || new Date().toLocaleDateString()
     date1 = new Date(date1).getTime()
     date2 = new Date(date2).getTime()
-
     if( date1>date2){
         return new Date(randomValueBetween(date2,date1)).toLocaleDateString()   
     } else{
